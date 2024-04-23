42bd9741ced4c2b0f550e235d23085e0f757133b
Deva <theprojessa@gmail.com>
2024-04-23 13:11:53 +0700

    feat: tambah service api wilayah

---
+++ resources/js/Services/getRegency.js
@@ -0,0 +1,26 @@
+export const getRegencys = async (code) => {
+    let url = `/regencys?province=${code}`;
+    const options = {
+        method: "GET",
+        url: url,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    try {
+        const response = await axios.request(options);
+        const dataArray = response.data.map((e) => ({
+            ...e,
+            code: e.code,
+            name: e.name
+                .toLowerCase()
+                .split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" "),
+        }));
+        return dataArray;
+    } catch (error) {
+        console.log(error);
+    }
+};
